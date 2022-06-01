58fcd67af38274faedf86e050ca0bf49a92b4c8c
YuigaWada <yuigawada@gmail.com>
2022-06-02 00:24:48 +0900

    [update] refine

--- installer.sh
+++ installer.sh
@@ -1,37 +1,41 @@
 TEMP="__installer_temp"
 
-if [ "`whoami`" != "root" ]; then
-  echo "Require root privilege"
-  exit 1
-fi
-
 # zsh
-cp ~/.zsh/.templaterc ~/.zshrc
+if [ ! -f /bin/zsh ]; then
+    apt install -y zsh
+    chsh -s /bin/zsh
+    echo "\n\n[TODO]\n1.Run [chsh -s /bin/zsh]. \n\n2.Restart your terminal and execute this scripts again.\n"
+    exit 0
+fi
+sudo cp $HOME/.zsh/.templaterc $HOME/.zshrc
 
 # workspace
 if [ ! -d $HOME/workspace ]; then
     mkdir $HOME/workspace
 fi
-
 cd $HOME/workspace
-mkdir $TEMP ; cd $TEMP
+mkdir $TEMP
+cd $TEMP
 
-# font 
+# font
 git clone https://github.com/romkatv/powerlevel10k-media
 cd powerlevel10k-media/
 if [ ! -d $HOME/.fonts ]; then
-    mkdir $HOME/.fonts
+    sudo mkdir $HOME/.fonts
 fi
-mv *.ttf $HOME/.fonts
+sudo mv *.ttf $HOME/.fonts
 
 # zplug
 if [ ! -d $HOME/.zplug ]; then
-    cp $HOME/.zsh/.zplug $HOME/.zplug
+    sudo apt update
+    sudo apt install zplug
+    source ~/.zplug/init.zsh
 fi
 
 # powerlevel10k
 if [ ! -d $HOME/powerlevel10k ]; then
-    cp $HOME/.zsh/powerlevel10k $HOME/powerlevel10k
+    zplug romkatv/powerlevel10k, as:theme, depth:1
+    sudo cp -r $HOME/.zsh/powerlevel10k $HOME/powerlevel10k
 fi
 
 # git
@@ -39,10 +43,10 @@ cp $HOME/.zsh/.gitconfig $HOME/.gitconfig
 cd $HOME/workspace/$TEMP
 git clone https://github.com/holygeek/git-number
 cd git-number
-cp git-* /usr/local/bin
+sudo cp git-* /usr/local/bin
 
 echo "\e[1;33m[TODO] If Terminator user. \n 1. Open Preferences using the context menu. \n\n 2.Under Profiles select the General tab (should be selected already), uncheck Use the system fixed width font (if not already) and select MesloLGS NF Regular. \n\n 3.Exit the Preferences dialog by clicking Close.\e[0m\n"
 rm -rf $HOME/workspace/$TEMP
 
-cp $HOME/.zsh/p10k-instant-prompt-yuigawada.zsh ${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh
-source ~/.zshrc
+# cp $HOME/.zsh/powerlevel10k/p10k-instant-prompt-yuigawada.zsh ${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh
+source $HOME/.zshrc
